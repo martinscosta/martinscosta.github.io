47b14854bca5f23300daf0504076cc2add2956c7
Marco Aurelio Martins Costa <martinscosta@mp.rs.gov.br>
2016-04-07 16:54:36 -0300

    Incluído o react-router

---
+++ app/components/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
+const About = () => {
+  return (
+    <Jumbotron>
+      Vários testes e desenvolvimentos realizados por mim. 
+    </Jumbotron>
+  );
+};
+
+export default About; 
--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'
 
 import App from './containers/App';
+import About from './components/pages/About';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('root'))
